b62763df46e5caa21c3be3e10da3fe9ce81b19fc
cam <cameronayres123@gmail.com>
2025-09-18 23:06:33 +0100

    Added lean4-mode and its dependencies, still need to document it.

--- init.el
+++ init.el
@@ -131,7 +131,14 @@
 (use-package geiser-guile
   :hook ((scheme-mode . geiser)))
 
-(use-package proof-general)
+(use-package dash)
+(use-package lsp-mode)
+
+(use-package lean4-mode
+  :commands lean4-mode
+  :straight (lean4-mode :type git :host github
+			:repo "leanprover-community/lean4-mode"
+			:files ("*.el" "data")))
 
 (use-package haskell-mode
   :bind
